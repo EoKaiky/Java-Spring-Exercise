31b917772407f9e60f90f71c09aa6858fcb7654c
EoKaiky <kaikybr234@gmail.com>
2025-03-19 10:46:31 -0300

    salvando alterações

--- TabelaFipe/src/main/java/br/com/study/TabelaFipe/principal/Principal.java
+++ TabelaFipe/src/main/java/br/com/study/TabelaFipe/principal/Principal.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
     public void exibeMenu() {
         var menu = """
                 *** OPÇÕES ***
@@ -16,5 +18,18 @@ public class Principal {
 
         System.out.println(menu);
         var opcao = leitura.nextLine();
+
+        String endereco;
+
+        if(opcao.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+
+
     }
 }
